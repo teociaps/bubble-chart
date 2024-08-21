cf45408a37bf82e518f7aafa7cd345495702dc5c
Matteo Ciapparelli <ciapparellimatteo@gmail.com>
2024-08-21 13:42:37 +0200

    Add rollup terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.ts',
@@ -12,6 +13,7 @@ export default {
   plugins: [
     resolve(),
     commonjs(),
-    typescript()
+    typescript(),
+    terser()
   ]
 };
