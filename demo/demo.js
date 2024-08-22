382c9ac15f7f994775da176b207b3ab1b3ecdb33
Matteo Ciapparelli <ciapparellimatteo@gmail.com>
2024-08-23 00:42:07 +0200

    Added demo folder

---
+++ demo/demo.js
@@ -0,0 +1,27 @@
+// Example:
+const data = [
+  { name: 'JavaScript', value: 100, color: 'yellow', icon: 'https://icon.icepanel.io/Technology/svg/JavaScript.svg' },
+  { name: 'Python', value: 90, color: 'blue', icon: 'https://icon.icepanel.io/Technology/svg/Python.svg'},
+  { name: 'Java', value: 86, color: 'green', icon: 'https://icon.icepanel.io/Technology/svg/Java.svg' },
+  { name: 'C++', value: 75, color: 'red', icon: 'https://icon.icepanel.io/Technology/svg/C%2B%2B-%28CPlusPlus%29.svg' },
+  { name: 'Ruby', value: 25, color: 'purple', icon: 'https://icon.icepanel.io/Technology/svg/Ruby.svg' },
+  { name: 'PHP', value: 10, color: 'orange', icon: 'https://icon.icepanel.io/Technology/svg/PHP.svg' },
+  { name: 'VisualBasic', value: 45, color: 'fuchsia' },
+  { name: 'C#', value: 37, color: 'cyan', icon: 'https://icon.icepanel.io/Technology/svg/C%23-%28CSharp%29.svg' }
+];
+
+const customBubbleChartOptions = {
+  titleOptions: {
+    text: 'Custom Bubble Chart',
+    fontSize: '24px',
+    fontWeight: 'bold',
+    fill: 'white',
+    fontFamily: 'Arial'
+  },
+  showPercentages: true
+};
+
+// Generate the SVG content
+const svgContent = createBubbleChart(data, customBubbleChartOptions, 'body', 1000, 700);
+
+console.log(svgContent);
